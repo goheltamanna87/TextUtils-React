3c16929d6aa8ce50d658028f54e43b527bca8aa8
Tamanna Gohel <tamannagohel32006@gmail.com>
2026-05-21 18:34:46 +0530

    add code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
 import Alert from "./components/Alert";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -38,17 +34,13 @@ function App() {
     }
   }
   return (
-   
-      <Router>
+      <>
         <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode}  />
         <Alert alert={alert}/>
         <div className="container my-3">
-            <Routes>
-              <Route exact path="/about" element={<About />} />
-              <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>} />
-        </Routes>
+          <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
         </div>
-        </Router>
+        </>
   
   );
 }
